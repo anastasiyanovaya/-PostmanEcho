5abab459b2bdcc525d2a5c4367177e6ae93760ad
anastasiynovaya <aefremova3014@gmail.com>
2022-10-01 22:17:28 +0500

    Test 1

---
+++ src/test/java/PostmanTest1.java
@@ -0,0 +1,27 @@
+package ru.netology.rest;
+
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanTest1 {
+    @Test
+    void statusCodeTest() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some value") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                 .body("data", equalTo("some value"))
+        ;
+    }
+}
